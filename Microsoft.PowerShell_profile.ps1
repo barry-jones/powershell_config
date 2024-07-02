4448d55c782209a83318362c0b7a27f898e19a43
Barry Jones <github@barryjones.me.uk>
2024-07-02 22:10:28 +0100

    moved config of ohmyposh to same directory

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,10 @@
+$config = join-path $PSScriptRoot "ohmyposhv3.json"
+
 Import-Module posh-git
 Import-Module PSReadLine 
 Import-Module -Name Terminal-Icons
 
-oh-my-posh --init --shell pwsh --config "C:\Users\barry\OneDrive\Documents\ohmyposhv3.json" | Invoke-Expression
+oh-my-posh --init --shell pwsh --config $config | Invoke-Expression
 
 Set-PSReadLineOption -PredictionSource History 
 Set-PSReadLineOption -PredictionViewStyle ListView 
